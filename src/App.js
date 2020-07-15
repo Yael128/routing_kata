c5a8bcf08d1c1d919788cf2edab15cafa9c4c3d2
manuel <manuelpirez@gmail.com>
2020-07-15 08:58:10 -0500

    routing_kata 1: instructions

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Routing Kata</h1>
+      <ol>
+        <li>Install react-router-dom</li>
+        <li>Create Home Component add a simple title and two links 1.Home 2.Topics</li>
+        <li>Create 2 Components Topics and Topic</li>
+        <li>Links in Home component should take you to Topics</li>
+        <li>In Topics add 3 list elements each one with a different topic name and use a Switch (router) to handle the route</li>
+        <li>Add Topic as a sub component for Topics and add a paragraph that receives the topic id and shows it to the screen </li>
+        <li>All link interactions have to use routing. and be sure that the pages don't reload everytime you click a link (there's a function in Router to prevent that) </li>
+      </ol>
     </div>
   );
 }
